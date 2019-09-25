f602b2246a04000cfba92e2a94ea0ccb5a1c40c2
yadav-vikas <52526748+yadav-vikas@users.noreply.github.com>
2019-09-25 13:38:42 +0530

    Create Modifying DataFrames.py

---
+++ Modifying DataFrames.py
@@ -0,0 +1,18 @@
+#this is data frames based on LAMBDA FUNCTION OF PYTHON
+#shows how to extract data from table using functions
+
+import codecademylib
+import pandas as pd
+
+orders = pd.read_csv('shoefly.csv')
+
+print(orders.head(5))
+
+orders['shoe_source'] = orders.shoe_material.apply(lambda x: \
+                        	'animal' if x == 'leather'else 'vegan')
+
+orders['salutation'] = orders.apply(lambda row: \
+                                    'Dear Mr. ' + row['last_name']
+                                    if row['gender'] == 'male'
+                                    else 'Dear Ms. ' + row['last_name'],
+                                    axis=1)
