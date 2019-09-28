c56179926954ac9c9e663833e29f615ef74d0b57
yadav-vikas <52526748+yadav-vikas@users.noreply.github.com>
2019-09-29 02:49:59 +0530

    Add files via upload

---
+++ data_analysis_with_mutiple_dataframes/test.py
@@ -0,0 +1,15 @@
+import codecademylib
+load_file_in_context('script.py')
+
+try:
+  v_to_c
+except:
+  fail_tests('Did you remember to define `v_to_c`?')
+  
+ans = pd.merge(visits, checkouts)
+
+if set(ans.columns) != set(v_to_c.columns):
+  fail_tests('Are you sure you merged correctly? You should have columns: {}, but you have columns {}'.format(
+      ', '.join(ans.columns), ', '.join(v_to_c.columns)))
+
+pass_tests()
---
+++ data_analysis_with_mutiple_dataframes/test3.py
@@ -0,0 +1,7 @@
+import codecademylib
+load_file_in_context('script.py')
+
+if not 'time' in v_to_c:
+  fail_tests('Did you remember to define `time`?')
+else:
+  pass_tests()
