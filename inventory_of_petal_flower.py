918709d5b988d148cbe64289c8da75697806528a
yadav-vikas <52526748+yadav-vikas@users.noreply.github.com>
2020-07-13 23:55:30 +0530

    Update inventory_of_petal_flower.py

--- inventory_of_petal_flower.py
+++ inventory_of_petal_flower.py
@@ -18,3 +18,32 @@ combine_lambda = lambda row: \
     '{} - {}'.format(row.product_type,
                      row.product_description)
 inventory['full_description']=combine_lambda
+
+
+
+
+------------------------------------------------------------------------------------------------
+import codecademylib
+import pandas as pd
+df= pd.read_csv('inventory.csv')
+print(df.head(10))
+staten_island=df[df.location== 'Staten Island']
+
+product_request = staten_island.product_description
+
+seed_request = df[(df.location =='Brooklyn') & (df.product_type =='seeds')]
+
+inventory=df
+
+inventory['in_stock'] = inventory.quantity.apply(lambda x:True if x >0 else False)
+
+inventory['total_value'] = inventory.price * inventory.quantity
+
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+
+inventory['full_description'] = inventory.apply(combine_lambda,axis=1)
+print(inventory)
+
+
